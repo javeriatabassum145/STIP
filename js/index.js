706a290bc613e341491a0cdff5415345c88be2ec
javeriatabassum145 <javeria.tabassum145@gmail.com>
2021-07-04 21:52:57 +0500

    final version

---
+++ js/index.js
@@ -0,0 +1,29 @@
+// jquery AOS
+
+AOS.init();
+AOS.init({
+    offset: 100, // offset (in px) from the original trigger point
+    // delay: 0, // values from 0 to 3000, with step 50ms
+    duration: 1500, // values from 0 to 3000, with step 50ms
+    easing: 'ease', // default easing for AOS animations
+    once: false, // whether animation should happen only once - while scrolling down
+    mirror: false, // whether elements should animate out while scrolling past them
+    anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+
+});
+
+
+// This function is made to shrink the navbar when user scroll the website 
+
+// Start of funtion 
+$(document).on("scroll", function () {
+    if
+        ($(document).scrollTop() > 30) {
+        $("#navbar-shrink").addClass("shrink");
+
+    }
+    else {
+        $("#navbar-shrink").removeClass("shrink");
+    }
+});
+// End of function 
